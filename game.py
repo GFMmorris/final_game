5871eca149a02219d918fa0724d4b8118d8429b7
GFMmorris <115820403+GFMmorris@users.noreply.github.com>
2022-11-15 10:54:09 -0500

    Game screen
    Building a game window that takes up the whole screen. Full screen mode and using the esc key to quit.

---
+++ game.py
@@ -0,0 +1,28 @@
+# File that runs the video game
+# imports 
+import sys
+from time import sleep
+
+import pygame
+
+
+class EscapeSpace:
+"""General class for the entire game. this will run all important methods and functions for the game."""
+    def __init(self):
+        """initialize pygame for the game space"""
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
+    def _check_events(self):
+        """Respond to key presses and mouse events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                self._check_keydown_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to key-presses."""
+        if event.key == pygame.K_q:
+            sys.exit()
