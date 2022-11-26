065a8814202ce9cf1d156e3e8f5f1518c250d2ca
GFMmorris <115820403+GFMmorris@users.noreply.github.com>
2022-11-26 13:34:29 -0500

    map update
    Now there is a background

--- map.py
+++ map.py
@@ -2,35 +2,45 @@ import pygame
 
 # 0 is fill_dirt
 
-grid = [
-    [0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0],
-    [],
-    [1],
-    [0, 0, 0, 0, 0, 0, 0],
-    [],
-    [],
-    [1],
-    [0, 0, 0, 0, 0, 0, 0]
+sky = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
+    [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
 ]
 
 TILE_SIZE = 64
 
+blue_sky = pygame.image.load('images/Background/background_0000.png')
+blue_sky = pygame.transform.scale(blue_sky, (TILE_SIZE, TILE_SIZE))
+cloud_top = pygame.image.load('images/Background/background_0001.png')
+cloud_top = pygame.transform.scale(cloud_top, (TILE_SIZE, TILE_SIZE))
+cloud = pygame.image.load('images/Background/background_0002.png')
+cloud = pygame.transform.scale(cloud, (TILE_SIZE, TILE_SIZE))
 
-dirt_fill = pygame.image.load('images/Tiles/fill_dirt.png')
-dirt_fill = pygame.transform.scale(dirt_fill, (TILE_SIZE,TILE_SIZE))
-bg_dirt_left = pygame.image.load('images/Tiles/bg_dirt_left.png')
-bg_dirt_left = pygame.transform.scale(bg_dirt_left, (TILE_SIZE, TILE_SIZE))
-
-tiles = [dirt_fill, bg_dirt_left]
+# Sets each type of image to a value inside a list. Then uses a list of lists to build the map above.
+sky_tiles = [blue_sky, cloud_top, cloud]
 
 
 def draw_background(bg_size):
     bg = pygame.Surface(bg_size)
     # draw each tile in the bg
 
-    for r, grid_list in enumerate(grid):
+    for r, grid_list in enumerate(sky):
         for t, grid_element in enumerate(grid_list):
             # blit the correct tile on the screen
-            bg.blit(tiles[grid_element], (t * TILE_SIZE, r * TILE_SIZE))
+            bg.blit(sky_tiles[grid_element], (t * TILE_SIZE, r * TILE_SIZE))
     return bg
